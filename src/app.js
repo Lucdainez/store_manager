b661fe9fd3795ce2d86de3c703a9bb9d21d6cd26
Lucas Dainez <lucdainez@gmail.com>
2022-11-11 17:24:38 -0300

    feat(app.js): criar rota
    foi criado a rota de /products

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 
+const productsLayerRouter = require('./routers');
+
+const { productsRouter } = productsLayerRouter;
+
 const app = express();
+app.use(express.json());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/products', productsRouter);
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
