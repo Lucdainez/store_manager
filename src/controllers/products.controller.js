37537a8b3020546d207ca3124fd5d18340049939
Lucas Dainez <lucdainez@gmail.com>
2022-11-12 14:45:40 -0300

    feat(products.controller.js): adicionar requisição
    temos uma função chamada addProduct que é um POST para adicionar um novo produto ao banco de dados e retorna um objeto com o nome e id do novo produto

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -16,7 +16,16 @@ const getProductId = async (req, res) => {
   return res.status(200).json(message);
 };
 
+const addProduct = async (req, res) => {
+  const { name } = req.body;
+  const { type, message } = await productsService
+    .addProduct(name);
+  if (type) return res.status(type).json({ message });
+  return res.status(201).json(message);
+};
+
 module.exports = {
   listProducts,
   getProductId,
+  addProduct,
 };
