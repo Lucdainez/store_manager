1a1c4c3af9c817964255cb9cd40b0c152759c927
Lucas Dainez <lucdainez@gmail.com>
2022-11-12 14:54:51 -0300

    feat(schema.js): adicionar joi
    usando joi para verificar um name passado para camada service

--- src/services/validations/schema.js
+++ src/services/validations/schema.js
@@ -1,7 +1,10 @@
 const joi = require('joi');
 
-const idSchema = joi.number().integer().min(1).required(); 
+const idSchema = joi.number().integer().min(1).required();
+
+const addProductSchema = joi.string().min(5).required();
 
 module.exports = {
   idSchema,
+  addProductSchema,
 };
