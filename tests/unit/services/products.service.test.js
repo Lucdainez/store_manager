e08b9b94b95cdce0e5833edd433552f8c8ec8b12
Lucas Dainez <lucdainez@gmail.com>
2022-11-12 17:34:25 -0300

    test: adicionar cobertura de testes
    foi testado se em caso de sucesso o objeto é retornado com sucesso e em caso de falha é enviado um erro

--- tests/unit/services/products.service.test.js
+++ tests/unit/services/products.service.test.js
@@ -2,8 +2,8 @@ const { expect } = require('chai');
 const sinon = require('sinon');
 const productsModel = require('../../../src/models/products.model');
 const { productsService } = require('../../../src/services');
-const { findAll, findById } = productsService;
-const { ALL_PRODUCTS, PRODUCT_ID } = require('./mocks/products.mock');
+const { findAll, findById, addProduct } = productsService;
+const { ALL_PRODUCTS, PRODUCT_ID, ADD_NEW_PRODUCT } = require('./mocks/products.mock');
 
 describe('Testes de unidades da camada service products', function () {
   afterEach(sinon.restore);
@@ -36,6 +36,22 @@ describe('Testes de unidades da camada service products', function () {
       const { type, message } = await findById(1);
       expect(type).to.deep.equal(null);
       expect(message).to.deep.equal(PRODUCT_ID);
+    });
+
+    it('retorna um objeto com novo produto e ID', async function () {
+      sinon.stub(productsModel, 'insertOneProduct').resolves([{ insertId: 4 }]);
+      sinon.stub(productsModel, 'findByProductsId').resolves(ADD_NEW_PRODUCT);
+      const { type, message } = await addProduct('produtoX');
+      expect(type).to.deep.equal(null);
+      expect(message).to.deep.equal(ADD_NEW_PRODUCT);
+    });
+
+    it('retorna erro caso objeto tenha menos de 5 caracteres', async function () {
+      sinon.stub(productsModel, 'insertOneProduct').resolves([{ insertId: 4 }]);
+      sinon.stub(productsModel, 'findByProductsId').resolves(undefined);
+      const { type, message } = await addProduct('prod');
+      expect(type).to.deep.equal(422);
+      expect(message).to.deep.equal(message);
     });
 
   });
