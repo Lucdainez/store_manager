bdc0a258fdbd8598afed58511cf5371813cc87e1
Lucas Dainez <lucdainez@gmail.com>
2022-11-12 12:01:26 -0300

    test(mocks): todos os mocks dos testes das camadas controller, service e model

---
+++ tests/unit/models/mocks/products.mock.js
@@ -0,0 +1,24 @@
+const ALL_PRODUCTS = [
+  {
+    id: 1,
+    name: "Martelo de Thor"
+  },
+  {
+    id: 2,
+    name: "Traje de encolhimento"
+  },
+  {
+    id: 3,
+    name: "Escudo do Capitão América"
+  }
+];
+
+const PRODUCT_ID = {
+  id: 1,
+  name: "Martelo de Thor"
+}
+
+module.exports = {
+  ALL_PRODUCTS,
+  PRODUCT_ID,
+}
