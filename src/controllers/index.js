8d87a25cf98d69ce9678e362dee2a28ebf7bf9e9
Lucas Dainez <lucdainez@gmail.com>
2022-11-15 20:02:46 -0300

    feat(index dos Controllers): adicionar importação de salesController ao index

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,7 @@
 const productsController = require('./products.controller');
+const salesController = require('./sales.controller');
 
 module.exports = {
   productsController,
+  salesController,
 };
