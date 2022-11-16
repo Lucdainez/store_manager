02bffc6ea8d37724cc458525bee93271765dd476
Lucas Dainez <lucdainez@gmail.com>
2022-11-16 18:54:38 -0300

    feat(sales.router.js): adicionar rota do tipo PUT

--- src/routers/sales.router.js
+++ src/routers/sales.router.js
@@ -12,4 +12,6 @@ router.get('/:id', salesController.requestSaleId);
 
 router.delete('/:id', salesController.deleteSale);
 
+router.put('/:id', salesController.updateSale);
+
 module.exports = router;
