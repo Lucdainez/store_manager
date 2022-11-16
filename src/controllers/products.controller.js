6b22402cb8b32819e7d089c45525d0716a5ad746
Lucas Dainez <lucdainez@gmail.com>
2022-11-16 14:24:43 -0300

    feat(products.controller.js): adicionar requisição ao controller
    temos uma requisição do tipo PUT para atualizar um produto

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -24,8 +24,18 @@ const addProduct = async (req, res) => {
   return res.status(201).json(message);
 };
 
+const setProduct = async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const { type, message } = await productsService
+    .setProduct(name, id);
+  if (type) return res.status(type).json({ message });
+  return res.status(200).json(message);
+};
+
 module.exports = {
   listProducts,
   getProductId,
   addProduct,
+  setProduct,
 };
