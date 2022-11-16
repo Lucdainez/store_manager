a95f068cfb964ef1a2928956773ac543ac046233
Lucas Dainez <lucdainez@gmail.com>
2022-11-16 14:35:35 -0300

    feat(products.model.js): adicionar função que entra em contato com o banco de dados
    essa função "updateProduct" altera um valor já existente no banco de dados e já o retorna para mandar para a requisição

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -23,8 +23,24 @@ const insertOneProduct = async (product) => {
   return insertId;
 };
 
+const updateProduct = async (name, id) => {
+  await connection.execute(
+    `UPDATE StoreManager.products
+    SET name = ?
+    WHERE id = ?;`,
+    [name, id],
+  );
+
+  const [[result]] = await connection.execute(
+    'SELECT * FROM StoreManager.products WHERE id = ?;',
+    [id],
+  );
+  return result;
+};
+
 module.exports = {
   findAllProducts,
   findByProductsId,
   insertOneProduct,
+  updateProduct,
 };
