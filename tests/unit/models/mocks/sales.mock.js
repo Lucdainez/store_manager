8c4c2d7d41cb9b02e563c5f55cc36c901e03253d
Lucas Dainez <lucdainez@gmail.com>
2022-11-17 16:27:51 -0300

    feat(sales.mock.js): mocks de retorno adicionados

---
+++ tests/unit/models/mocks/sales.mock.js
@@ -0,0 +1,38 @@
+const ALL_SALES = [
+  {
+    "saleId": 1,
+    "date": "2022-11-17T16:21:55.000Z",
+    "productId": 1,
+    "quantity": 5
+  },
+  {
+    "saleId": 1,
+    "date": "2022-11-17T16:21:55.000Z",
+    "productId": 2,
+    "quantity": 10
+  },
+  {
+    "saleId": 2,
+    "date": "2022-11-17T16:21:55.000Z",
+    "productId": 3,
+    "quantity": 15
+  }
+];
+
+const REQUEST_SALE_ID_1 = [
+  {
+    "date": "2022-11-17T16:21:55.000Z",
+    "productId": 1,
+    "quantity": 5
+  },
+  {
+    "date": "2022-11-17T16:21:55.000Z",
+    "productId": 2,
+    "quantity": 10
+  }
+];
+
+module.exports = {
+  ALL_SALES,
+  REQUEST_SALE_ID_1,
+};
