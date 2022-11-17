58d92899af28924683ef4cc1a79724de5334d6bb
Lucas Dainez <lucdainez@gmail.com>
2022-11-17 16:02:55 -0300

    test(products.model.test.js): cobertura de testes da products model 100%

--- tests/unit/models/products.model.test.js
+++ tests/unit/models/products.model.test.js
@@ -2,7 +2,7 @@ const { expect } = require('chai');
 const sinon = require('sinon');
 const { productsModel } = require('../../../src/models/index');
 const connection = require('../../../src/models/connection');
-const { ALL_PRODUCTS, PRODUCT_ID } = require('./mocks/products.mock');
+const { ALL_PRODUCTS, PRODUCT_ID, ALTERED_PRODUCT } = require('./mocks/products.mock');
 
 describe('Testes de unidades da camada model products', function () {
   afterEach(sinon.restore);
@@ -24,5 +24,29 @@ describe('Testes de unidades da camada model products', function () {
     const result = await productsModel.insertOneProduct('produtoX')
     expect(result).to.equal(4);
   });
-});
 
+  it('alterando um produto', async function () {
+    sinon.stub(connection, 'execute').resolves([[ALTERED_PRODUCT]]);
+    const result = await productsModel.updateProduct('produtoX')
+    expect(result).to.be.deep.equal(ALTERED_PRODUCT);
+  });
+
+  it('Deletando um produto', async function () {
+    sinon.stub(connection, 'execute').resolves();
+    const result = await productsModel.deleteProduct(1)
+    expect(result).to.be.deep.equal();
+  });
+
+  it('Busca de produto por parâmetro query sem valor', async function () {
+    sinon.stub(connection, 'execute').resolves([ALL_PRODUCTS]);
+    const result = await productsModel.searchProductQuery()
+    expect(result).to.be.deep.equal(ALL_PRODUCTS);
+  });
+
+  it('Busca de produto por parâmetro query com valor', async function () {
+    sinon.stub(connection, 'execute').resolves([ALL_PRODUCTS[0]]);
+    const result = await productsModel.searchProductQuery('Martelo')
+    expect(result).to.be.deep.equal(ALL_PRODUCTS[0]);
+  });
+
+});
