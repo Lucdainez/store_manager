913d8ac1312cdd4ece9edb0f2fd6deecdd2fc431
Lucas Dainez <lucdainez@gmail.com>
2022-11-17 12:18:20 -0300

    feat(products.mocks.js): adicionar mocks de requisição da rota /products

--- tests/unit/controllers/mocks/products.mock.js
+++ tests/unit/controllers/mocks/products.mock.js
@@ -23,8 +23,30 @@ const ADD_NEW_PRODUCT = {
   name: "produtoX"
 }
 
+const ALTERED_PRODUCT = {
+  id: 4,
+  name: "produtoXs"
+}
+
+const RETURN_PRODUCT_PARAM_QUERY_A = [
+  {
+    "id": 1,
+    "name": "Martelo de Thor"
+  },
+  {
+    "id": 2,
+    "name": "Traje de encolhimento"
+  },
+  {
+    "id": 3,
+    "name": "Escudo do Capitão América"
+  }
+]
+
 module.exports = {
   ALL_PRODUCTS,
   PRODUCT_ID,
   ADD_NEW_PRODUCT,
+  ALTERED_PRODUCT,
+  RETURN_PRODUCT_PARAM_QUERY_A,
 }
